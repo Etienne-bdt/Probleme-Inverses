dbb33dbd228b8a6e6cbbb4ba8a694204b0ac5c40
Etienne <etienne.bardet@gmail.com>
2024-05-27 21:29:30 +0200

    update

--- main.m
+++ main.m
@@ -17,26 +17,22 @@ V = normrnd(0,1,1,J);
 
 a = Bw.*V;
 
-x = @(n) sum(a.*cos(2*pi*fj*n),2);
-
-Xval = zeros(1,N);
-for i=1:N
-    Xval(i) = x(i);
-end
+x = sum(a.*cos(2*pi*fj.*(1:N)'),2);
 
 
 %% Affichage de x
 
 
-plot(1:N, Xval)
+%plot(1:N, x)
 
 %%
-Sx = abs(fft(Xval)).^2;
+Sx = abs(fft(x)).^2;
+
 
 
 figure;
 semilogy(Sx)
-
+legend('Densité spectrale de x')
 %% Creation de H
 
 Iwav = 7;
@@ -46,36 +42,36 @@ Lwav = 9;
 Hfull = convmtx(h',N);
 H = Hfull(Lwav/2:end-Lwav/2,:);
 
+vp_H = eig(H);
+cond = max(abs(vp_H))/min(abs(vp_H))
 %% Génération de y
 
-ybar = H*Xval';
+ybar = H*x;
 
 SNRdB = 20;
-Px = 1/N*norm(Xval)^2;
+Px = 1/N*norm(x)^2;
 sigma2 = 10^(-SNRdB/10)*Px;
 b = sqrt(sigma2)*randn(N,1);
 y_noisy = ybar+b;
 
-vp_H = eig(H);
-cond = max(abs(vp_H))/min(abs(vp_H))
 
 %% Affichage de y
 figure;
 plot(y_noisy)
-
+legend('Signal y bruité')
 %% Least-Square Inversion
 
-xLS = y_noisy\H;
+xLS = H\y_noisy;
 figure;
 plot(xLS);
-
+legend('Inversion par moindres carrés')
 %% Periodo de x Least Square
 
 Sxls = abs(fft(xLS)).^2;
 
 figure;
-semilogy(Sxls);
-
+semilogy(nfft, Sx, nfft, Sxls);
+legend('DSP de x', 'DSP de $x_{LS}$','interpreter','latex')
 %Le problème est dur à résoudre, il y a un mauvais conditionnement, par
 %conséquent, la solution des moindres carrés ne marche pas bien
 
@@ -92,7 +88,7 @@ Pxreg = norm(RegMat,2);
 
 %% Regularisation
 
-Tlambda = 10.^(-5 : 0.2 : 5);
+Tlambda = 10.^(-5 : 0.2 : 2);
 Nlambda = length(Tlambda);
 Tab_MSE = zeros(1,Nlambda)+NaN;
 Tab_J = zeros(1,Nlambda)+NaN;
@@ -103,12 +99,12 @@ for lambda = Tlambda
     i = i+1;
     x_hat = (H'*H + lambda * (RegMat'*RegMat))\H'*y_noisy;
     
-    Tab_MSE(i) = norm(Xval'-x_hat)^2;
+    Tab_MSE(i) = norm(x-x_hat)^2;
     Tab_J(i) = norm(y_noisy-H*x_hat)^2+lambda * norm(RegMat*x_hat);
     
     % as a function of lambda
     subplot(311)
-    plot(vect_t,Xval,'g')
+    plot(vect_t,x,'g')
     hold on
     plot(vect_t,x_hat,'k')
     plot(vect_t,y_noisy)
@@ -152,9 +148,10 @@ legend('Sx','Sx chapeau')
 %% DCT based Regularization
 
 psi = dctmtx(N);
-dct = psi*Xval';
+dct = psi*x;
 figure;
 plot(dct)
+legend('DCT de x')
 
 %Beaucoup de coefficients sont proches de 0, on peut comprimer beaucoup une
 %image/un signal en négligeant ces coefficients
@@ -169,11 +166,33 @@ det_psi = det(psi)
 % ||y- Hx||^2 + ||psix||^2
 % HtH x + psiTpsi x
 
-lambda_dct2 = 1;
-x_hat_dct2 = (H'*H + lambda_dct2*eye(N))\H'*y_noisy;
-
+Tlambda_dct2 = 10.^(-5 : 0.2 : 1);
+Nlambda_dct2 = length(Tlambda_dct2);
+Tab_MSE_dct2 = zeros(1,Nlambda_dct2)+NaN;
 figure;
-plot(x_hat_dct2)
+i=0;
+for lambda_dct2 = Tlambda_dct2
+    i=i+1;
+    x_hat_dct2 = (H'*H + lambda_dct2*eye(N))\H'*y_noisy;
+    subplot(2,1,1)
+    plot(vect_t,x);
+    hold on
+    plot(vect_t,x_hat_dct2,'k')
+    axis tight
+    xlabel('t')
+    legend('x','$\hat{x}_{DCT l_2}$', 'interpreter', 'latex')
+    hold off
+    subplot(2,1,2)
+    Tab_MSE_dct2(i) = norm(x-x_hat_dct2)^2;
+    ylabel('$||x-\hat{x}_{\lambda}||_2^2$','interpreter','latex')
+    plot(1:Nlambda_dct2,Tab_MSE_dct2)
+    drawnow
+end
+
+[~ , i_opt_dct2] = min(Tab_MSE_dct2);
+lambda_opt_dct2 = Tlambda_dct2(i_opt_dct2);
+
+x_hat_dct2 = (H'*H + lambda_opt_dct2*eye(N))\H'*y_noisy;
 
 %% L1
 
@@ -181,7 +200,6 @@ plot(x_hat_dct2)
 
 % Ici, ca revient à faire ce qu'on voit slide 160, on résoud le problème
 % sur u (dct de x) pour ca on fait algo d'opti (voir BE opti pour ca)
-figure;
 Tlambda_dct = 10.^(-5 : 0.2 : 1);
 Nlambda_dct = length(Tlambda_dct);
 Tab_MSE_dct = zeros(1,Nlambda_dct)+NaN;
@@ -191,19 +209,20 @@ i=0;
 for lambda_dct = Tlambda_dct
     u = psi*y_noisy;
     i=i+1;
-    for l=1:100
+    for l=1:50
         u = ProxL1(u-s*(((H*psi')'*(H*psi'))*u-(H*psi')'*y_noisy),lambda_dct);
     end
     subplot(2,1,1)
-    plot(vect_t,Xval);
+    plot(vect_t,x);
     hold on
     plot(vect_t,psi'*u,'k')
     axis tight
     xlabel('t')
-    legend('x','x_hat')
+    legend('x','$\hat{x}_{DCT l_1}$','interpreter','latex')
     hold off
     subplot(2,1,2)
-    Tab_MSE_dct(i) = norm(Xval'-psi'*u)^2;
+    Tab_MSE_dct(i) = norm(x-psi'*u)^2;
+    ylabel('$||x-\Psi^Tu}||_2^2$','interpreter','latex')
     plot(1:Nlambda_dct,Tab_MSE_dct)
     drawnow
 end
@@ -216,36 +235,40 @@ lambda_opt_dct1 = Tlambda_dct(i_opt_dct);
 
 
 %% Iteration sur lambda opt
-k2max = 100;
+k2max = 50;
 k=1;
 u = psi*y_noisy;
 err_dct = zeros(1,k2max);
+figure;
 while k<=k2max
     u = ProxL1(u-s*(((H*psi')'*(H*psi'))*u-(H*psi')'*y_noisy),lambda_opt_dct1);
     subplot(2,1,1)
-    plot(vect_t,Xval);
+    plot(vect_t,x);
     hold on
     plot(vect_t,psi'*u,'k')
     axis tight
     xlabel('t')
-    legend('x','x_hat')
+    legend('x','$\Psi^Tu_{\lambda}$','interpreter','latex')
     hold off
     subplot(2,1,2)
-    err_dct(k) = norm(Xval - psi'*u)^2;
-    plot(Tab_MSE_dct)
+    err_dct(k) = norm(x - psi'*u)^2;
+    ylabel('$||x-\Psi^Tu_{\lambda}||_2^2$','interpreter','latex')
+    plot(err_dct)
+    k=k+1;
     drawnow
 end
 
+x_hat_dct1 = psi'*u;
 
 %% Comparaison
 
 figure;
 hold on
-plot(Xval)
+plot(x)
 plot(x_hat_dct2)
 plot(x_hat_dct1)
 hold off
-
+legend('x', '$\hat{x}_{DCT2}$','$\hat{x}_{DCT1}$','interpreter','latex')
 %% DSP
 
 Sx_hat_dct2 = abs(fft(x_hat_dct2)).^2;
@@ -253,9 +276,9 @@ Sx_hat_dct1 = abs(fft(x_hat_dct1)).^2;
 
 %% DSP comp
 
-figure;
-semilogy(nfft, Sx, nfft,  Sx_hat_dct2, nfft, Sx_hat_dct1)
-legend('Sx','Sx dct1' , 'Sx dct2')
+%figure;
+%semilogy(nfft, Sx, nfft,  Sx_hat_dct2, nfft, Sx_hat_dct1)
+%legend('Sx','Sx dct1' , 'Sx dct2')
 
 %% Dictionary-based regularizations
 K=2^9;
@@ -276,7 +299,7 @@ x_hat_dict2 = D*z_hat;
 
 figure;
 hold on
-plot(Xval)
+plot(x)
 plot(x_hat_dict2)
 hold off
 
@@ -285,7 +308,7 @@ hold off
 %% L1 mais lambda
 
 
-Tlambda_dict = 10.^(-3 : 0.2 : 1);
+Tlambda_dict = 10.^(-5 : 0.2 : 1);
 Nlambda_dict = length(Tlambda_dict);
 Tab_MSE_dict = zeros(1,Nlambda_dict)+NaN;
 i=0;
@@ -294,20 +317,21 @@ s=0.001;
 for lambda_dict = Tlambda_dict
     i = i+1;
     z = ones(K,1);
-    for l=1:20
+    for l=1:40
         z = ProxL1(z-s*(((H*D)'*(H*D))*z-(H*D)'*y_noisy),lambda_dict);
     end
     subplot(211)
+    plot(vect_t,x)
     hold on
     plot(vect_t,D*z,'k')
     axis tight
     xlabel('t')
-    legend('x','x_hat')
+    legend('x','$Dz l_1$','interpreter' ,'latex')
     hold off
     subplot(2,1,2)
-    Tab_MSE_dict(i) = norm(Xval'-D*z)^2;
+    Tab_MSE_dict(i) = norm(x-D*z)^2;
     plot(Tab_MSE_dict)
-    drawnow;
+    drawnow
 end
 
 
@@ -320,21 +344,24 @@ lambda_opt_dict1 = Tlambda_dict(i_opt_dict);
 
 k=1;
 z = ones(K,1);
-err_dic = zeros(1,kmax2);
+err_dic = zeros(1,k2max);
 figure;
-while k<=kmax2
+while k<=k2max
     z = ProxL1(z-s*(((H*D)'*(H*D))*z-(H*D)'*y_noisy),lambda_opt_dict1);
     subplot(2,1,1)
-    plot(vect_t,Xval);
+    plot(vect_t,x);
     hold on
     plot(vect_t,D*z,'k')
     axis tight
     xlabel('t')
-    legend('x','x_hat')
+    legend('x','$Dz_{\lambda l_1}$', 'interpreter','latex')
     hold off
     subplot(2,1,2)
-    err_dic(k) = norm(Xval-D*z)^2;
+    err_dic(k) = norm(x-D*z)^2;
+    ylabel('$||x-Dz_{\lambda}||_2^2$','interpreter','latex')
     k=k+1;
-    plot(1:kmax2, err_dic)
+    plot(1:k2max, err_dic)
     drawnow
 end
+
+x_hat_dict1 = D*z;
